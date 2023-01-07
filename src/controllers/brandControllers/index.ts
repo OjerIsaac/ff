1043b035a61c5083525f35213405003250312567
Ojer Isaac <isaacojerumu@gmail.com>
2023-01-07 08:40:05 +0100

    feat: meal controllers and routes boilerplate

--- src/controllers/brandControllers/index.ts
+++ src/controllers/brandControllers/index.ts
@@ -14,7 +14,102 @@ export const createNewMeal = async (req: Request, res: Response) => {
     const { name, description, price, category } = req.body;
 
     // check for duplicates
-    return successResponse(res, "Assessments retrieved successfully", {});
+    return successResponse(res, "New Meal created successfully", {});
+  } catch (error) {
+    console.log(error);
+    return errorResponse(res, httpErrors.ServerError, "Something went wrong");
+  }
+};
+
+
+/**
+ * @description Retrieve a list of all meal addons for the specified brand
+ * @param req Request object
+ * @param res Response object
+ * @returns ErrorResponse | SuccessResponse
+ */
+export const getAllMeal = async (req: Request, res: Response) => {
+  try {
+    const { brandId } = req.params;
+
+    // check for duplicates
+    return successResponse(res, "All Meals retrieved successfully", {});
+  } catch (error) {
+    console.log(error);
+    return errorResponse(res, httpErrors.ServerError, "Something went wrong");
+  }
+};
+
+
+/**
+ * @description Retrieve a single meal
+ * @param req Request object
+ * @param res Response object
+ * @returns ErrorResponse | SuccessResponse
+ */
+export const getOneMeal = async (req: Request, res: Response) => {
+  try {
+    const { brandId } = req.params;
+
+    // check for duplicates
+    return successResponse(res, "Meal retrieved successfully", {});
+  } catch (error) {
+    console.log(error);
+    return errorResponse(res, httpErrors.ServerError, "Something went wrong");
+  }
+};
+
+
+/**
+ * @description Update a single meal addon by its ID
+ * @param req Request object
+ * @param res Response object
+ * @returns ErrorResponse | SuccessResponse
+ */
+export const updateMeal = async (req: Request, res: Response) => {
+  try {
+    const { brandId } = req.params;
+    const { name, description, price, category } = req.body;
+
+    // check for duplicates
+    return successResponse(res, "Meal updated successfully", {});
+  } catch (error) {
+    console.log(error);
+    return errorResponse(res, httpErrors.ServerError, "Something went wrong");
+  }
+};
+
+/**
+ * @description Delete a single meal addon by its ID
+ * @param req Request object
+ * @param res Response object
+ * @returns ErrorResponse | SuccessResponse
+ */
+export const deleteMeal = async (req: Request, res: Response) => {
+  try {
+    const { brandId } = req.params;
+
+    // check for duplicates
+    return successResponse(res, "Meal successfully deleted", {});
+  } catch (error) {
+    console.log(error);
+    return errorResponse(res, httpErrors.ServerError, "Something went wrong");
+  }
+};
+
+/**
+ * @description Create a new category for meal addon
+ * @param req Request object
+ * @param res Response object
+ * @returns ErrorResponse | SuccessResponse
+ */
+export const createNewMealCategory = async (req: Request, res: Response) => {
+  try {
+    const { brandId } = req.params;
+    const { name } = req.body;
+
+    // check for duplicates
+    return successResponse(res, "New Meal category created successfully", {});
   } catch (error) {
     console.log(error);
     return errorResponse(res, httpErrors.ServerError, "Something went wrong");
--- src/routes/brandRoute.ts
+++ src/routes/brandRoute.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-import { createNewMeal } from '../controllers/brandControllers/index';
+import { createNewMeal, getAllMeal, getOneMeal, updateMeal, deleteMeal, createNewMealCategory } from '../controllers/brandControllers/index';
 
 export default (route: Router) => {
     route.post('/brands/:brandId/addons', createNewMeal);
-    route.get('/brands/:brandId/addons');
-    route.get('/brands/:brandId/addons/:addonId');
-    route.put('/brands/:brandId/addons/:addonId');
-    route.delete("/brands/:brandId/addons/:addonId");
-    route.post('/brands/:brandId/addon-categories');
+    route.get('/brands/:brandId/addons', getAllMeal);
+    route.get('/brands/:brandId/addons/:addonId', getOneMeal);
+    route.put('/brands/:brandId/addons/:addonId', updateMeal);
+    route.delete('/brands/:brandId/addons/:addonId', deleteMeal);
+    route.post('/brands/:brandId/addon-categories', createNewMealCategory);
 }
