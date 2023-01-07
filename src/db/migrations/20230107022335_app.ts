fec8257373a67a3d0b91b185347b5e6d6b7c1f9a
Ojer Isaac <isaacojerumu@gmail.com>
2023-01-07 07:22:12 +0100

    feat: added created_at & updated_at column to migration

--- src/db/migrations/20230107022335_app.ts
+++ src/db/migrations/20230107022335_app.ts
@@ -1,30 +1,36 @@
 import { Knex } from 'knex';
 
-async function createUsersTable(table: Knex.CreateTableBuilder) {
+async function createUsersTable(knex: Knex, table: Knex.CreateTableBuilder) {
   table.increments('id').primary();
   table.string('username').notNullable();
   table.string('email').unique().notNullable();
   table.string('role').notNullable();
+  table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable();
+  table.timestamp('updated_at').defaultTo(knex.fn.now()).notNullable();
 }
 
-async function createBrandsTable(table: Knex.CreateTableBuilder) {
+async function createBrandsTable(knex: Knex, table: Knex.CreateTableBuilder) {
   table.increments('id').primary();
   table.string('name').notNullable();
+  table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable();
+  table.timestamp('updated_at').defaultTo(knex.fn.now()).notNullable();
 }
 
-async function createMealsTable(table: Knex.CreateTableBuilder) {
-    table.increments('id').primary();
-    table.string('name').notNullable();
-    table.string('description').notNullable();
-    table.string('price').notNullable();
-    table.string('caategory').notNullable();
-    table.integer('brandId').unsigned().references('brands.id');
+async function createMealsTable(knex: Knex, table: Knex.CreateTableBuilder) {
+  table.increments('id').primary();
+  table.string('name').notNullable();
+  table.string('description').notNullable();
+  table.string('price').notNullable();
+  table.string('caategory').notNullable();
+  table.integer('brandId').unsigned().references('brands.id');
+  table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable();
+  table.timestamp('updated_at').defaultTo(knex.fn.now()).notNullable();
 }
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.schema.createTable('users', createUsersTable);
-  await knex.schema.createTable('brands', createBrandsTable);
-  await knex.schema.createTable('meals', createMealsTable);
+  await knex.schema.createTable('users', table => createUsersTable(knex, table));
+  await knex.schema.createTable('brands', table => createBrandsTable(knex, table));
+  await knex.schema.createTable('meals', table => createMealsTable(knex, table));
   console.log('Tables created!');
 }
 
